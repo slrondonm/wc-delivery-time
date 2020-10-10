5c1b755cd2a4596cb68a23dbcc4bc8364cd8fbf8
Sergio Rondón <sl.rondon.m@gmail.com>
2020-10-10 16:08:13 -0500

    Add Path Structure and Base Files

---
+++ wc-delivery-time.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Delivery Time
+ * Plugin URI: https://virtualizate.com.co/
+ * Description: Its a Description Short.
+ * Version: 0.1.0
+ * Author: Grupo Virtualizate. 
+ * Author URI: https://virtualizate.com
+ * 
+ * Developer: Franyeli Mendible | Ing. Sergio Rondon.
+ * Developer URI: https://github.com/slrondonm/wc-delivery-time
+ * 
+ * Text Domain: wc-delivery-time
+ * Domain Path: /languages
+ * 
+ * WC requires at least: 2.2
+ * WC tested up to: 2.3
+ * 
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
