22165d9ad024d98f07c30ce1c22bedebd2e4d12b
Sergio Rondón <sl.rondon.m@gmail.com>
2020-10-10 21:08:22 -0500

    Add Header php

--- includes/class-wcdt.php
+++ includes/class-wcdt.php
@@ -1,7 +1,29 @@
 <?php
 /**
- * 
- * 
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://virtualizate.com.co
+ * @since      0.1.0
+ *
+ * @package    WCDT
+ * @subpackage WCDT/includes
  */
 
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      0.1.0
+ * @package    WCDT
+ * @subpackage WCDT/includes
+ * @author     Ing. Sergio Rondón <soporte@virtualizate.com.co>
+ */
  
